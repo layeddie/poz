d839be6056b8b083b7352cb2fb17283af9e5f60a
Tymon Tobolski <i@teamon.eu>
2017-05-22 20:28:19 +0200

    init

---
+++ lib/poz.ex
@@ -0,0 +1,18 @@
+defmodule Poz do
+  @moduledoc """
+  Documentation for Poz.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Poz.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/poz_test.exs
@@ -0,0 +1,8 @@
+defmodule PozTest do
+  use ExUnit.Case
+  doctest Poz
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
